32cd3bef46bd73616f65a290592e24c69371eb7f
bravo <2385097162@qq.com>
2018-12-06 20:20:47 +0800

    fix:群主认证、合同模版

--- src/routes/Community/Certification.js
+++ src/routes/Community/Certification.js
@@ -1,62 +1,491 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'dva';
+import { routerRedux } from 'dva/router';
+// import moment from 'moment';
 import {
+  Row,
+  Col,
   Card,
   Form,
   Input,
+  Select,
+  Icon,
   Button,
+  message,
+  Modal,
+  // Dropdown,
+  // Menu,
+  // InputNumber,
+  DatePicker,
+  Badge,
+  Divider,
 } from 'antd';
+import StandardTable from 'components/StandardTable';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
-const FormItem = Form.Item;
+import styles from './TableList.less';
 
-const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 7 },
-    },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 15 },
-      md: { span: 15 },
-    },
-  };
-  const submitFormLayout = {
-    wrapperCol: {
-      xs: { span: 24, offset: 0 },
-      sm: { span: 10, offset: 7 },
-    },
-  };
-@connect(({ live, loading }) => ({
-  live,
-  loading: loading.models.live,
+const { confirm } = Modal;
+const FormItem = Form.Item;
+const InputGroup = Input.Group;
+const { Option } = Select;
+const getValue = obj =>
+  Object.keys(obj)
+    .map(key => obj[key])
+    .join(',');
+const statusMap = ['default', 'success', 'error'];
+// const statusMap = ['processing', 'processing', 'error'];
+const goodsStatus = ['未审核', '通过', '拒绝'];
+// const goodsTypeStatus = ['普通商品', '一元购', '秒杀', '众筹'];
+// const payType = ['拍下减库存', '付款减库存'];
+// const isTrue = ['否', '是'];
+
+@connect(({ classModel, loading }) => ({
+  classModel,
+  loading: loading.models.classModel,
 }))
 @Form.create()
-export default class Certification extends PureComponent {
-  state = {}
-  
-  render() {
+export default class ClassList extends PureComponent {
+  state = {
+    expandForm: false,
+    formValues: {},
+    minPrice: '',
+    maxPrice: '',
+    page: 1, // 页脚
+  };
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'classModel/getclassList',
+      payload: {
+        page_number: 3,
+      },
+    });
+  }
+
+  handleStandardTableChange = (pagination, filtersArg, sorter) => {
+    const { dispatch } = this.props;
+    const { formValues } = this.state;
+
+    const filters = Object.keys(filtersArg).reduce((obj, key) => {
+      const newObj = { ...obj };
+      newObj[key] = getValue(filtersArg[key]);
+      return newObj;
+    }, {});
+
+    // const params = {
+    //   currentPage: pagination.current,
+    //   pageSize: pagination.pageSize,
+    //   ...formValues,
+    //   ...filters,
+    // };
+    const params = {
+      page_number: pagination.pageSize,
+      ...formValues,
+      ...filters,
+      page: pagination.current,
+    };
+    if (sorter.field) {
+      params.sorter = `${sorter.field}_${sorter.order}`;
+    }
+    dispatch({
+      type: 'classModel/getclassList',
+      payload: params,
+    });
+  };
+
+  handleFormReset = () => {
+    const { form, dispatch } = this.props;
+    form.resetFields();
+    this.setState({
+      formValues: {},
+      minPrice: '',
+      maxPrice: '',
+    });
+    dispatch({
+      type: 'classModel/getclassList',
+      payload: {},
+    });
+  };
+
+  toggleForm = () => {
+    this.setState({
+      expandForm: !this.state.expandForm,
+    });
+  };
+
+
+
+  handleSearch = e => {
+    e.preventDefault();
+    const { page } = this.state;
+    const { dispatch, form } = this.props;
+
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+
+      const values = {
+        ...fieldsValue,
+        updatedAt: fieldsValue.updatedAt && fieldsValue.updatedAt.valueOf(),
+        page,
+      };
+
+      this.setState({
+        formValues: values,
+      });
+      dispatch({
+        type: 'classModel/getclassList',
+        payload: values,
+      });
+    });
+  };
+
+  // 删除商品
+  deleteItem = id => {
+    event.preventDefault();
+    const that = this;
+    confirm({
+      content: '你确定删除这个吗？',
+      okText: '确定',
+      okType: 'danger',
+      cancelText: '取消',
+      onOk() {
+        const { page } = that.state;
+        const { dispatch } = that.props;
+        dispatch({
+          type: 'classModel/deleteClass',
+          payload: id,
+          page: {
+            page,
+            page_number: 3,
+          },
+          callback: () => {
+            message.success('删除成功！');
+          },
+        });
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  };
+  // 新建商品
+  goNew = () => {
+    const { dispatch } = this.props;
+    const url = `/community/class-add`;
+    dispatch(routerRedux.push(url));
+  };
+
+  renderSimpleForm() {
     const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline" autoComplete="OFF">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="用户id">
+              {getFieldDecorator('title')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="姓名">
+              {getFieldDecorator('title')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          {/* <Col md={8} sm={24}>
+            <FormItem label="使用状态">
+              {getFieldDecorator('goods_status')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="0">上架中</Option>
+                  <Option value="1">未上架</Option>
+                  <Option value="2">下架</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col> */}
+          <Col md={8} sm={24}>
+            <span className={styles.submitButtons}>
+              <Button type="primary" htmlType="submit">
+                查询
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+                重置
+              </Button>
+              {/* <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+                展开 <Icon type="down" />
+              </a> */}
+            </span>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  renderAdvancedForm() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline" autoComplete="OFF">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="商品名称">
+              {getFieldDecorator('goods_name')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="使用状态">
+              {getFieldDecorator('goods_status')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="0">上架中</Option>
+                  <Option value="1">下架中</Option>
+                  <Option value="2">审核中</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="创建日期">
+              {getFieldDecorator('create_start')(
+                <DatePicker
+                  style={{ width: '100%' }}
+                  format="YYYY-MM-DD HH:mm:ss"
+                  showTime
+                  placeholder="请输入创建日期"
+                />
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="价格区间">
+              <InputGroup compact>
+                <Input
+                  key={1}
+                  value={this.state.minPrice}
+                  style={{ width: 70, textAlign: 'center' }}
+                  onChange={this.setMin}
+                  placeholder="Min"
+                />
+                <Input
+                  key={2}
+                  style={{
+                    width: 30,
+                    borderLeft: 0,
+                    pointerEvents: 'none',
+                    backgroundColor: '#fff',
+                  }}
+                  placeholder="~"
+                  disabled
+                />
+                <Input
+                  key={3}
+                  value={this.state.maxPrice}
+                  style={{ width: 70, textAlign: 'center', borderLeft: 0 }}
+                  onChange={this.setMax}
+                  placeholder="Max"
+                />
+              </InputGroup>
+            </FormItem>
+          </Col>
+          {/* <Col md={8} sm={24}>
+            <FormItem label="使用状态">
+              {getFieldDecorator('status3')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="0">关闭</Option>
+                  <Option value="1">运行中</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="使用状态">
+              {getFieldDecorator('status4')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="0">关闭</Option>
+                  <Option value="1">运行中</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col> */}
+        </Row>
+        <div style={{ overflow: 'hidden' }}>
+          <span style={{ float: 'right', marginBottom: 24 }}>
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+            <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+              重置
+            </Button>
+            <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
+              收起 <Icon type="up" />
+            </a>
+          </span>
+        </div>
+      </Form>
+    );
+  }
+
+  renderForm() {
+    return this.state.expandForm ? this.renderAdvancedForm() : this.renderSimpleForm();
+  }
+
+  render() {
+    const { classModel: { classList: datas, classListPage }, loading } = this.props;
+    const data = {
+      list: datas,
+      pagination: {
+        ...classListPage,
+      },
+    };
+
+    const columns = [
+      {
+        title: '会员',
+        dataIndex: 'member',
+        key: 'member',
+        render: (val, text) => (
+          // <Row style={{ width: 500 }}>
+          //   <Col span={4}>
+          //     <img style={{ height: 80, width: 80 }} src={val} alt="头像" />
+          //   </Col>
+          //   <Col span={14} style={{ fontSize: 14 }}>
+          //     <div>{text.nickname}</div>
+          //     <div>Id:{text.id}</div>
+          //     <div>等级:{text.account_level}</div>
+          //     <div>上级:{text.referee && text.referee.nickname}</div>
+          //   </Col>
+          // </Row>
+          <div className={styles.userBox}>
+            <img style={{ height: 80, width: 80 }} src={val} alt="头像" />
+            <div className={styles.userInfo}>
+              <div>{text.nickname}</div>
+              <div>Id:{text.id}</div>
+              <div>等级:{text.account_level}</div>
+              {/* <div className={styles.superior} onClick={this.searchMsg.bind(this, text.referee)} >上级:{text.referee && text.referee.nickname}</div> */}
+              <div>手机号码:{text.mobile}</div>
+            </div>
+          </div>
+        ),
+      },
+      // {
+      //   title: '课程价格',
+      //   dataIndex: 'price',
+      //   key: 'price',
+      // },
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+        // render: (val) => {
+        //   return val === 1 ? '单课' :`${val}集`;
+        // },
+      },
+      {
+        title: '身份证号',
+        dataIndex: 'id_num',
+        key: 'id_num',
+        // render: (val) => <img style={{ height: 80, width: 80 }} src={val} alt="" />,
+      },
+      {
+        title: '身份证正面照',
+        dataIndex: 'id_front_img',
+        key: 'id_front_img',
+        render: (val) => <img style={{ height: 80, width: 80 }} src={val} alt="身份证正面照" />,
+      },
+      {
+        title: '身份证反面照',
+        dataIndex: 'id_back_img',
+        key: 'id_back_img',
+        render: (val) => <img style={{ height: 80, width: 80 }} src={val} alt="身份证反面照" />,
+      },
+      {
+        title: '手持身份证照',
+        dataIndex: 'id_people_img',
+        key: 'id_people_img',
+        render: (val) => <img style={{ height: 80, width: 80 }} src={val} alt="手持身份证照" />,
+      },
+      {
+        title: '状态',
+        dataIndex: 'goods_status',
+        key: 'goods_status',
+        filters: [
+          {
+            text: goodsStatus[0],
+            value: 0,
+          },
+          {
+            text: goodsStatus[1],
+            value: 1,
+          },
+          {
+            text: goodsStatus[2],
+            value: 2,
+          },
+        ],
+        filterMultiple: false,
+        onFilter: (value, record) => record.goods_status.toString() === value,
+        render(val) {
+          return <Badge status={statusMap[val]} text={goodsStatus[val]} />;
+        },
+      },
+      {
+        title: '更新时间',
+        dataIndex: 'update_time',
+        key: 'update_time',
+      },
+      // {
+      //   title: '剩余库存',
+      //   dataIndex: 'goods_total_inventory',
+      //   render: (val, record) => val - record.goods_total_sales,
+      // },
+      // {
+      //   title: '商品上架时间',
+      //   dataIndex: 'goods_shelves_time',
+      //   sorter: true,
+      //   render: val => <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>,
+      // },
+      {
+        title: '操作',
+        fixed: 'right',
+        width: 150,
+        render: record => (
+          <Fragment>
+            <a href={`#/community/class-edit/${record.id}`}>同意</a>
+            <Divider type="vertical" />
+            <a onClick={this.deleteItem.bind(this, record.id)}>驳回</a>
+          </Fragment>
+        ),
+      },
+    ];
+
+    // const menu = (
+    //   <Menu onClick={this.handleMenuClick} selectedKeys={[]}>
+    //     <Menu.Item key="remove">删除</Menu.Item>
+    //     <Menu.Item key="approval">批量审批</Menu.Item>
+    //   </Menu>
+    // );
+
     return (
       <PageHeaderLayout>
         <Card bordered={false}>
-          <Form autoComplete="OFF">
-            <FormItem {...formItemLayout} label="分类名称">
-              {getFieldDecorator('name', {
-                rules: [
-                    {
-                    required: true,
-                    message: '请输入分类名称',
-                    },
-                ],
-              })(<Input placeholder="给分类起个名字" />)}
-            </FormItem>
-            <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
-              <Button type="primary" >
-                修改
+          <div className={styles.tableList}>
+            <div className={styles.tableListForm}>{this.renderForm()}</div>
+            {/* <div className={styles.tableListOperator}>
+              <Button icon="plus" type="primary" onClick={this.goNew.bind(this)}>
+                新建
               </Button>
-            </FormItem>
-          </Form>
+            </div> */}
+            <StandardTable
+              rowKey={record => record.id}
+              scroll={{ x: 1400 }}
+              selectedRows={false}
+              loading={loading}
+              data={data}
+              columns={columns}
+              onSelectRow={this.handleSelectRows}
+              onChange={this.handleStandardTableChange}
+            />
+          </div>
         </Card>
       </PageHeaderLayout>
     );
--- src/routes/Community/Contract.js
+++ src/routes/Community/Contract.js
@@ -1,76 +1,258 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { connect } from 'dva';
-import {
-  Table,
-  Card,
-  Form,
-  Button,
-  Row,
-  Col,
-  Input,
-} from 'antd';
-import PageHeaderLayout from '../../layouts/PageHeaderLayout';
-import styles from './TableList.less';
+// import { Form, Button, Input, Select, Upload, Icon, Modal, Tag, message, InputNumber } from 'antd';
+import { Form, Button, Input, Upload, Icon, Tag, message, Card } from 'antd';
+import styles from './style.less';
+import Wangeditor from '../../components/Wangeditor';
+// import { router } from 'sw-toolbox';
+// import LiveGoodTable from '../../../components/LiveGoodTable';
 
 const FormItem = Form.Item;
-@connect(({ live, loading }) => ({
-  live,
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 7 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 12 },
+    md: { span: 12 },
+  },
+};
+const submitFormLayout = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 10, offset: 7 },
+  },
+};
+
+const CustomizedForm = Form.create({
+  onFieldsChange(props, changedFields) {
+    props.onChange(changedFields);
+  },
+  mapPropsToFields(props) {
+    const arr = {
+      front: Form.createFormField({
+        value: props.protocolForm.front,
+      }),
+      back: Form.createFormField({
+        value: props.protocolForm.back,
+      }),
+      people: Form.createFormField({
+        value: props.protocolForm.people,
+      }),
+      desc: Form.createFormField({
+        value: props.protocolForm.desc,
+      }),
+    };
+    return arr;
+  },
+  // onValuesChange(_, values) {
+  //   console.log(values);
+  // },
+})(props => {
+  //  111
+  const { getFieldDecorator, validateFields } = props.form;
+  const onValidateForm = e => {
+    e.preventDefault();
+    const { submitForm } = props;
+    validateFields((err, values) => {
+      if (!err) {
+        submitForm(values);
+      } else {
+        message.error('请填写信息');
+      }
+    });
+  };
+  const {
+    uploadUrl,
+    protocolForm,
+    // handleChangeImg,
+    header,
+  } = props;
+  // const headers = {
+  //   token,
+  // }
+
+  // 上传按钮
+  const uploadButton = (
+    <div>
+      <Icon type="plus" />
+      <div className="ant-upload-text">图片</div>
+    </div>
+  );
+  // 上传图片参数
+  const payload = {
+    type: 2,
+  };
+  //  限制大小
+  const beforeUpload = (file) => {
+    const isLt1M = file.size / 1024 / 1024 < 1;
+    if (!isLt1M) {
+      message.error('图片不能超过1M!');
+    }
+    return isLt1M;
+  }
+  return (
+    <Form autoComplete="OFF">
+      <Form.Item
+        className={styles.extraTag}
+        {...formItemLayout}
+        label="身份证正面"
+        extra={<Tag color="blue">大小不得大于1M, 示例<img className="img" src="https://314live.image.alimmdn.com/5/2/154409110713.jpeg" alt="图片" /></Tag>}
+      >
+        {getFieldDecorator('front', {
+          valuePropName: 'fileList',
+          getValueFromEvent(...args) {
+            const { fileList, file } = args[0];
+            if (!file.status) {
+              const list = fileList.filter(res => {
+                return res.uid !== file.uid;
+              });
+              return list;
+            }
+            return fileList;
+          },
+          rules: [{ required: true }],
+        })(
+          <Upload
+            action={uploadUrl}
+            beforeUpload={beforeUpload}
+            listType="picture-card"
+            // onPreview={handlePreviewImg}
+            data={payload}
+            headers={header}
+          >
+            {protocolForm.front.length ? null : uploadButton}
+          </Upload>
+        )}
+      </Form.Item>
+      <Form.Item
+        className={styles.extraTag}
+        {...formItemLayout}
+        label="身份证反面"
+        extra={<Tag color="blue">大小不得大于1M</Tag>}
+      >
+        {getFieldDecorator('back', {
+          valuePropName: 'fileList',
+          getValueFromEvent(...args) {
+            const { fileList, file } = args[0];
+            if (!file.status) {
+              const list = fileList.filter(res => {
+                return res.uid !== file.uid;
+              });
+              return list;
+            }
+            return fileList;
+          },
+          rules: [{ required: true }],
+        })(
+          <Upload
+            action={uploadUrl}
+            beforeUpload={beforeUpload}
+            listType="picture-card"
+            // onPreview={handlePreviewImg}
+            data={payload}
+            headers={header}
+          >
+            {protocolForm.back.length ? null : uploadButton}
+          </Upload>
+        )}
+      </Form.Item>
+      <Form.Item
+        className={styles.extraTag}
+        {...formItemLayout}
+        label="手持证件半身照"
+        extra={<Tag color="blue">大小不得大于1M</Tag>}
+      >
+        {getFieldDecorator('people', {
+          valuePropName: 'fileList',
+          getValueFromEvent(...args) {
+            const { fileList, file } = args[0];
+            if (!file.status) {
+              const list = fileList.filter(res => {
+                return res.uid !== file.uid;
+              });
+              return list;
+            }
+            return fileList;
+          },
+          rules: [{ required: true }],
+        })(
+          <Upload
+            action={uploadUrl}
+            beforeUpload={beforeUpload}
+            listType="picture-card"
+            // onPreview={handlePreviewImg}
+            data={payload}
+            headers={header}
+          >
+            {protocolForm.people.length ? null : uploadButton}
+          </Upload>
+        )}
+      </Form.Item>
+      <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
+        <Button type="primary" htmlType="submit" onClick={onValidateForm}>
+          提交
+        </Button>
+      </FormItem>
+    </Form>
+  );
+});
+
+@connect(({ protocol, loading }) => ({
+  protocol,
   loading: loading.models.live,
 }))
-@Form.create()
-export default class Contract extends PureComponent {
-    
-  
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    const { loading } = this.props;
-    const datas = [{
-        id: 1,
-        title: '323',
-    }];
-    const liveListPage = {};
-    // const { getFieldDecorator } = this.props.form;
-    const progressColumns = [
-      {
-        title: '直播标题',
-        dataIndex: 'title',
-        key: 'title',
-      },
-    ];
+// @Form.create()
+class EditVodStep2 extends React.PureComponent {
+  state = {
+    header: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  }
+  componentDidMount() {
+  }
+  // 新增修改提交
+  submitForm = () => {
 
+  };
+  // 修改表单值
+  changeFormVal = val => {
+    const { dispatch } = this.props;
+    const obj = {};
+    for (const key of Object.keys(val)) {
+      obj[key] = val[key].value;
+    }
+    dispatch({
+      type: 'protocol/changeFormVals',
+      payload: {
+        obj,
+      },
+    });
+  };
+  render() {
+    const { uploadUrl, protocol: { protocolForm } } = this.props;
+    const { header } = this.state;
     return (
-      <PageHeaderLayout>
-        <Card bordered={false}>
-          <div className={styles.tableListForm}>
-            <Form  autoComplete="OFF">
-              <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
-                <Col md={8} sm={24}>
-                  <FormItem label="商品名称">
-                    {getFieldDecorator('goods_name')(<Input placeholder="请输入" />)}
-                  </FormItem>
-                </Col>
-                <Col md={8} sm={24}>
-                  <span className={styles.submitButtons}>
-                    <Button type="primary" htmlType="submit">
-                      查询
-                    </Button>
-                    <Button style={{ marginLeft: 8 }} >
-                      重置
-                    </Button>
-                  </span>
-                </Col>
-              </Row>
-            </Form>
-          </div>
-          <Table
-            dataSource={datas}
-            rowKey={record => record.id}
-            loading={loading}
-            columns={progressColumns}
-            pagination={liveListPage}
+      <div>
+        <Card>
+          <p>请按照提示填写本人真实的资料</p>
+          <CustomizedForm
+            protocolForm={protocolForm}
+            uploadUrl={uploadUrl}
+            onChange={this.changeFormVal}
+            header={header}
+            handleShareImg={this.handleShareImg}
+            submitForm={this.submitForm}
           />
         </Card>
-      </PageHeaderLayout>
+      </div>
     );
   }
 }
+
+export default connect(({ form, loading }) => ({
+  submitting: loading.effects['goods/addShop'],
+  data: form.step,
+}))(EditVodStep2);
